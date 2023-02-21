5ffd0864efa5a2b48458fe947adaed7098777303
BrianGoga21 <gogabrian01@gmail.com>
2023-02-21 12:10:17 -0800

    3 Alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,18 +3,18 @@
  * main - main block
  * print the alphabet in lower caps
  * and then in uppercase, follow by a new line
- * return: 0
+ * return: 0 (Success)
  */
 int main(void)
 {
 	char c = 'a';
+
 
 	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
 	}
-
 	c = 'A';
 
 	while (c <= 'z')
@@ -24,5 +24,5 @@ int main(void)
 	}
 
 	putchar('\n');
-	retun(0)
+	return(0);
 }
