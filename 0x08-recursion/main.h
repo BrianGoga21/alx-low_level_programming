2730b1e13071980113730137a340153c6c7c7ab6
BrianGoga21 <gogabrian01@gmail.com>
2023-03-13 11:56:27 -0700

    Write a function that creates an array of chars, and initializes it with a specific char

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,3 +11,23 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 #endif
+
+#ifndef MAIN_H
+
+#define MAIN_H
+
+char *create_array(unsigned int size, char c);
+
+int _putchar(char c);
+
+char *_strdup(char *str);
+
+char *str_concat(char *s1, char *s2);
+
+int **alloc_grid(int width, int height);
+
+void free_grid(int **grid, int height);
+
+char *argstostr(int ac, char **av);
+
+#endif
